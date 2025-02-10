43431c3526f15372dd278dcce98d66ccee831236
Anjali Burnwal <anjali.burnwal@tuteck.com>
2025-02-10 15:21:54 +0530

    login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import loginClass from './css/login.module.css';
 import { useNavigate } from 'react-router-dom';
 import Swal from "sweetalert2";
-
+import { Container, Navbar, Nav, NavDropdown, Button, Badge, Row, Col, Card, Carousel, Form, FormControl, InputGroup, } from 'react-bootstrap';
 
 
 function LoginForm() {
@@ -42,39 +41,198 @@ function LoginForm() {
   };
 
 
- return (
-  <div>
-      <h2>Login</h2>
-      <div>
-        <label>Username:</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
+  return (
+    <div className="loginbackground w-100 h-100">
+      <Container>
+        <Navbar expand="lg" className="bg-transparent py-3 custom-badge d-flex align-items-center">
+          <Navbar.Brand className="navbrand" href="#">
+            <img src="src\assets\logo.svg" className="img-fluid pe-4" alt="" width="200" height="61.66" />
+          </Navbar.Brand>
+          <Navbar.Brand className="navbrand2" href="#">
+            <img src="src\assets\NSS_logo.png" className="img-fluid border-0" alt="" width="120" height="61.66" />
+          </Navbar.Brand>
+          <Badge className="badge badge-outline badge-danger px-3">ASI</Badge>
+          <Nav className="ms-auto">
+            <Button variant="primary" className="rounded-pill px-4 d-flex align-items-center justify-content-center">
+              <i class="bi bi-person"></i>
+              <span className="login-text ms-1">Login</span>
+            </Button>
+          </Nav>
+        </Navbar>
+        <Navbar expand="lg" className="custom-loginnavbar my-2 p-1">
+          <Navbar.Toggle aria-controls="navbarToggler">
+            <i className="bi bi-list"></i>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="navbarToggler">
+            <Nav className="custom-loginnavbar-nav d-flex justify-content-between align-items-center w-100" variant="pills" defaultActiveKey="/home">
+              <Nav.Item className="custom-loginnavbar-nav-item py-2 px-1">
+                <i className="bi bi-house-door me-2"></i>Home
+              </Nav.Item>
+              <Nav.Item className="custom-loginnavbar-nav-item py-2 px-1">
+                <i className="bi bi-arrow-counterclockwise me-2"></i>History
+              </Nav.Item>
+              <Nav.Item className="custom-loginnavbar-nav-item py-2 px-1">
+                <i className="bi bi-grid me-2"></i>Present Structure
+              </Nav.Item>
+              <Nav.Item className="custom-loginnavbar-nav-item py-2 px-1">
+                <i className="bi bi-gear me-2"></i>Functions
+              </Nav.Item>
+              <NavDropdown className="custom-loginnavbar-nav-item" title={<><i className="bi bi-info-circle me-2"></i>About</>}>
+                <NavDropdown.Item className="custom-loginnavbar-nav-item-dropdown" href="#">Lorem Ipsum</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item className="custom-loginnavbar-nav-item-dropdown" href="#">Lorem Ipsum</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Item className="custom-loginnavbar-nav-item py-2 px-1">
+                <i className="bi bi-card-text me-2"></i>ASI Manual
+              </Nav.Item>
+              <Nav.Item className="custom-loginnavbar-nav-item py-2 px-1">
+                <i className="bi bi-circle me-2"></i>NIC
+              </Nav.Item>
+              <Nav.Item className="custom-loginnavbar-nav-item py-2 px-1">
+                <i className="bi bi-circle me-2"></i>NPCMS
+              </Nav.Item>
+              <Nav.Item className="custom-loginnavbar-nav-item py-2 px-1">
+                <i className="bi bi-bank me-2"></i>State IIP
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Row className="mb-5 mt-3">
+          <Col md={7}>
+            <Card className="leftcard shadow-none">
+              <Card.Header as="h5" className="leftcard-header p-0 pb-4 m-0 border-0">National Statistical Office - Industrial Statistics Wing</Card.Header>
+              <Card.Body className="p-0 leftcard-body">
+                <Carousel id="login-carousel" className="custom-carousel">
+                  <Carousel.Item className="custom-carousel-inneritem active">
+                    <img
+                      src="src\assets\hero_image_1.avif"
+                      className="d-block w-100"
+                      alt=""
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item className="custom-carousel-inneritem">
+                    <img
+                      src="src\assets\hero_image_2.avif"
+                      className="d-block w-100"
+                      alt=""
+                    />
+                  </Carousel.Item>
+                </Carousel>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={5}>
+            <Card className="rightcard py-4 px-5">
+              <Card.Header className="rightcard-header p-0">
+                <h4 className="rightcard-header-h4">Welcome Back !</h4>
+                <h3 className="rightcard-header-title">Login</h3>
+              </Card.Header>
+              <Card.Body className="p-0 rightcard-body">
+                <Row>
+                  <Col md={12} sm={12}>
+                    <Form.Group controlId="formUsername" className="formGroup  mb-3">
+                      <Form.Label className="formGroup-formLabel">Username:</Form.Label>
+                      <FormControl
+                        type="text"
+                        className="formGroup-formcontrol"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                        placeholder="Enter Username"
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col md={12} sm={12}>
+                    <Form.Group controlId="formPassword" className="formGroup  mb-3">
+                      <Form.Label className="formGroup-formLabel">Password:</Form.Label>
+                      <FormControl
+                        type="password"
+                        className="formGroup-formcontrol"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        placeholder="Enter Password"
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col md={12} sm={12}>
+                    <InputGroup className="mb-3 logininput-group">
+                      <FormControl
+                        id="generated-captcha"
+                        value={captcha}
+                        onChange={(e) => setCaptchaInput(e.target.value)}
+                        onMouseDown={(e) => e.preventDefault()}
+                        onSelectStart={(e) => e.preventDefault()}
+                        required
+                        readOnly
+                      />
+                      <InputGroup.Text className="p-0 border-0">
+                      <Button type="button" className="d-flex align-items-center btn btn-primary" onClick={refreshCaptcha}><i className="bi bi-arrow-clockwise"></i></Button>
+                      </InputGroup.Text>
+                    </InputGroup>
+                  </Col>
+                  <Col md={12} sm={12}>
+                    <FormControl
+                      className="mb-3"
+                      type="text"
+                      id="entered-captcha"
+                      placeholder="Enter Above captcha.."
+                      value={captchaInput}
+                      onChange={(e) => setCaptchaInput(e.target.value)}
+                      required
+                      />
+                      {errorMessage && <div className="alert alert-danger"><i class="bi bi-exclamation-circle me-2"></i>{errorMessage}</div>}
+                  </Col>
+                  <Col md={12} sm={12} className="my-3">
+                    <button className="btn-block py-2" onClick={handleSubmit}>Login</button>
+                  </Col>
+                 {/*  <Col className="col-md-12 col-sm-12">
+                    <div className="forgetpasswordlink d-flex justify-content-end">
+                      <a href="#">Forgot Password?</a>
+                    </div>
+                  </Col> */}
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      <div className="loginfooter">
+        <div className="loginfooter-first py-3 px-3">
+          <Nav className="loginfooter-first-nav">
+            <Nav.Item className="loginfooter-first-nav-navitem">
+              <Nav.Link className="loginfooter-first-nav-navitem-link py-0 pe-4" href="#">RTI</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="loginfooter-first-nav-navitem">
+              <Nav.Link className="loginfooter-first-nav-navitem-link py-0 pe-4" href="#">Public</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="loginfooter-first-nav-navitem">
+              <Nav.Link className="loginfooter-first-nav-navitem-link py-0 pe-4" href="#">Notice</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="loginfooter-first-nav-navitem">
+              <Nav.Link className="loginfooter-first-nav-navitem-link py-0 pe-4" href="#">Photo</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="loginfooter-first-nav-navitem">
+              <Nav.Link className="loginfooter-first-nav-navitem-link py-0 pe-4" href="#">Gallery</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="loginfooter-first-nav-navitem">
+              <Nav.Link className="loginfooter-first-nav-navitem-link py-0 pe-4" href="#">Telephone Directory</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="loginfooter-first-nav-navitem">
+              <Nav.Link className="loginfooter-first-nav-navitem-link py-0 pe-4" href="#">Feedback</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="loginfooter-first-nav-navitem">
+              <Nav.Link className="loginfooter-first-nav-navitem-link py-0 pe-4" href="#">Disclaimer</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </div>
+        <div className="loginfooter-second">
+          <div class="container">
+            <p class="mb-0 py-2">© 2025, National Statistical Office - Industrial Statistics Wing. All rights reserved.
+              Ministry of Statistics & Programme Implementation</p>
+          </div>
+        </div>
       </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Captcha: {captcha}</label>
-        <input
-          type="text"
-          value={captchaInput}
-          onChange={(e) => setCaptchaInput(e.target.value)}
-          required
-        />
-        {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
-        <button type="button" onClick={refreshCaptcha}>Refresh Captcha</button>
-      </div>
-      <button onClick={handleSubmit}>Login</button>
     </div>
   );
 }
